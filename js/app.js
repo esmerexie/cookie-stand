80236954c4b8b3d26971814291d6d9497259964e
Rexie Esmeralda <esmeraldarexie@gmail.com>
2022-06-19 22:23:59 -0700

    finshed data output and form responsiveness

--- js/app.js
+++ js/app.js
@@ -1,156 +1,90 @@
 'use strict';
 
 let hours = ['6am','7am', '8am','9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+let storeFormEl = document.getElementById('cookieForm');
 
-let seattle = {
-  storeName: 'seattle',
-  minCust: 23,
-  maxCust: 65,
-  avgCPC: 6.3,
-  custAEH: [],
-  totalSales: 0,
-  hourlyCust: [],
+storeFormEl.addEventListener('submit', function(event) {
 
-  generateRan: function(){
-    let randomNum = Math.round(Math.random() * (this.maxCust - this.minCust) + this.minCust);
-    return randomNum;
-  },
-  generateHourlyCust: function(){
-    for (let i = 0; i < hours.length; i++){
-      let randomNum = this.generateRan();
-      this.hourlyCust.push(randomNum);
-    }
-    console.log(this.hourlyCust);
-  },
-  generateCookieSale: function(){
-    this.generateHourlyCust();
-    for (let i = 0; i < hours.length; i++){
-      this.custAEH.push(Math.ceil(this.hourlyCust[i] * this.avgCPC));
-    }
+  event.preventDefault();
+
+  let name = event.target.location.value;
+  let minCust = parseInt(event.target.min.value);
+  let maxCust = parseInt(event.target.max.value);
+  let avgCookies = parseInt(event.target.avgCookies.value);
+
+  let store = new Store(name, minCust, maxCust, avgCookies);
+  store.generateCookiesPerHour();
+  store.renderRow();
+});
+
+
+function generateNumberBetween(min, max) {
+  return Math.round(Math.random() * (max - min)) + min;
+}
+
+function Store(name, minCust, maxCust, avgCookiePerCust) {
+  this.name = name;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.hourlySales = [];
+  this.storeHours = hours;
+  this.totalSales = 0;
+  this.avgCookiePerCust = avgCookiePerCust;
+}
+
+Store.prototype.generateCookiesPerHour = function() {
+
+  for (let i = 0; i < hours.length; i++) {
+    let customersAtHour = generateNumberBetween(this.minCust, this.maxCust);
+    let salesAtHour = Math.round(customersAtHour * this.avgCookiePerCust);
+    this.hourlySales.push(salesAtHour);
+    this.totalSales += salesAtHour;
   }
+
 };
 
-let tokyo = {
-  storeName: 'tokyo',
-  minCust: 3,
-  maxCust: 24,
-  avgCPC: 1.2,
-  custAEH: [],
-  totalSales: 0,
-  hourlyCust: [],
+// function to render table and data //
+Store.prototype.renderRow = function() {
 
-  generateRan: function(){
-    let randomNum = Math.round(Math.random() * (this.maxCust - this.minCust) + this.minCust);
-    return randomNum;
-  },
-  generateHourlyCust: function(){
-    for (let i = 0; i < hours.length; i++){
-      let randomNum = this.generateRan();
-      this.hourlyCust.push(randomNum);
-    }
-    console.log(this.hourlyCust);
-  },
-  generateCookieSale: function(){
-    this.generateHourlyCust();
-    for (let i = 0; i < hours.length; i++){
-      this.custAEH.push(Math.ceil(this.hourlyCust[i] * this.avgCPC));
-    }
+  let tableEl = document.getElementById('table-area');
+  let rowEl = document.createElement('tr');
+
+  let cellName = document.createElement('td');
+  cellName.textContent = this.name;
+  rowEl.appendChild(cellName);
+
+  for (let i = 0; i < this.hourlySales.length; i++) {
+    let cellEl = document.createElement('td');
+    cellEl.textContent = this.hourlySales[i];
+    rowEl.appendChild(cellEl);
   }
+
+  let totalCell = document.createElement('td');
+  totalCell.textContent = this.totalSales;
+  rowEl.appendChild(totalCell);
+
+  tableEl.appendChild(rowEl);
 };
 
-let dubai = {
-  storeName: 'dubai',
-  minCust: 11,
-  maxCust: 38,
-  avgCPC: 3.7,
-  custAEH: [],
-  totalSales: 0,
-  hourlyCust: [],
+// setting the new variable that adds to the store function //
+let seattle = new Store('Seattle', 23, 65, 6.3);
+let tokyo = new Store('Tokyo', 3, 24, 1.2);
+let dubai = new Store('Dubai', 11, 38, 3.7);
+let paris = new Store('Paris', 20, 38, 2.3);
+let lima = new Store('Lima', 2, 16, 4.6);
 
-  generateRan: function(){
-    let randomNum = Math.round(Math.random() * (this.maxCust - this.minCust) + this.minCust);
-    return randomNum;
-  },
-  generateHourlyCust: function(){
-    for (let i = 0; i < hours.length; i++){
-      let randomNum = this.generateRan();
-      this.hourlyCust.push(randomNum);
-    }
-    console.log(this.hourlyCust);
-  },
-  generateCookieSale: function(){
-    this.generateHourlyCust();
-    for (let i = 0; i < hours.length; i++){
-      this.custAEH.push(Math.ceil(this.hourlyCust[i] * this.avgCPC));
-    }
-  }
-};
+// calls the function to generate and display store data//
+seattle.generateCookiesPerHour();
+seattle.renderRow();
 
-let paris = {
-  storeName: 'paris',
-  minCust: 20,
-  maxCust: 38,
-  avgCPC: 2.3,
-  custAEH: [],
-  totalSales: 0,
-  hourlyCust: [],
+tokyo.generateCookiesPerHour();
+tokyo.renderRow();
 
-  generateRan: function(){
-    let randomNum = Math.round(Math.random() * (this.maxCust - this.minCust) + this.minCust);
-    return randomNum;
-  },
-  generateHourlyCust: function(){
-    for (let i = 0; i < hours.length; i++){
-      let randomNum = this.generateRan();
-      this.hourlyCust.push(randomNum);
-    }
-    console.log(this.hourlyCust);
-  },
-  generateCookieSale: function(){
-    this.generateHourlyCust();
-    for (let i = 0; i < hours.length; i++){
-      this.custAEH.push(Math.ceil(this.hourlyCust[i] * this.avgCPC));
-    }
-  }
-};
+dubai.generateCookiesPerHour();
+dubai.renderRow();
 
-let lima = {
-  storeName: 'lima',
-  minCust: 2,
-  maxCust: 16,
-  avgCPC: 4.6,
-  custAEH: [],
-  totalSales: 0,
-  hourlyCust: [],
+paris.generateCookiesPerHour();
+paris.renderRow();
 
-  generateRan: function(){
-    let randomNum = Math.round(Math.random() * (this.maxCust - this.minCust) + this.minCust);
-    return randomNum;
-  },
-  generateHourlyCust: function(){
-    for (let i = 0; i < hours.length; i++){
-      let randomNum = this.generateRan();
-      this.hourlyCust.push(randomNum);
-    }
-    console.log(this.hourlyCust);
-  },
-  generateCookieSale: function(){
-    this.generateHourlyCust();
-    for (let i = 0; i < hours.length; i++){
-      this.custAEH.push(Math.ceil(this.hourlyCust[i] * this.avgCPC));
-    }
-  }
-};
-
-let tableSpace = document.getElementById('table');
-let row1 = document.createElement('tr');
-let column = document.createElement('td');
-
-column.textContent = hours,
-
-console.log(row1, tableSpace);
-
-row1.appendChild(column);
-tableSpace.appendChild(row1);
-
-console.log(seattle);
+lima.generateCookiesPerHour();
+lima.renderRow();
